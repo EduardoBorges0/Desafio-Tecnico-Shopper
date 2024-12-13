e786e1add00b1c97f8828452d588151621c37ff8
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:06:48 -0300

    Update TripDTO.kt

--- app/src/main/java/com/app/testetcnicoshopper/Model/DTO/TripDTO/TripDTO.kt
+++ app/src/main/java/com/app/testetcnicoshopper/Model/DTO/TripDTO/TripDTO.kt
@@ -1,3 +1,57 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cde128a2e61801dc8887d8afac1c1dabf36928b076e8a99b6b0a7432ab24556d
-size 1679
+package com.app.testetcnicoshopper.Model.DTO.TripDTO
+
+import com.app.testetcnicoshopper.Model.entities.TripEntities.Driver
+import com.app.testetcnicoshopper.Model.entities.TripEntities.TripModel
+
+data class TripDTO(
+    val customer_id: String,
+    val origin: String?,
+    val destination: String?,
+    val distance: Double,
+    val duration: String,
+    val driver: DriverTripDTO, // Agora é um único objeto DriverTripDTO
+    val value: Double
+)
+
+data class DriverTripDTO(
+    val id: Int,
+    val name: String
+)
+
+fun Driver.toDriverTripDTO(): DriverTripDTO {
+    return DriverTripDTO(
+        id = this.id,
+        name = this.name
+    )
+}
+
+fun DriverTripDTO.toDriver(): Driver {
+    return Driver(
+        id = this.id,
+        name = this.name
+    )
+}
+
+fun TripModel.toDTO(): TripDTO {
+    return TripDTO(
+        customer_id = this.customer_id,
+        origin = this.origin,
+        destination = this.destination,
+        distance = this.distance,
+        duration = this.duration,
+        driver = this.driver.toDriverTripDTO(), // Ajustado para refletir um único objeto
+        value = this.value
+    )
+}
+
+fun TripDTO.toModel(): TripModel {
+    return TripModel(
+        customer_id = this.customer_id,
+        origin = this.origin,
+        destination = this.destination,
+        distance = this.distance,
+        duration = this.duration,
+        driver = this.driver.toDriver(), // Ajustado para refletir um único objeto
+        value = this.value
+    )
+}
