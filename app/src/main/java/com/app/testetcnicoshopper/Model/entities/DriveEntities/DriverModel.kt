b35d624c7f06c8766da6697954b2c128719b540d
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:07:46 -0300

    Update DriverModel.kt

--- app/src/main/java/com/app/testetcnicoshopper/Model/entities/DriveEntities/DriverModel.kt
+++ app/src/main/java/com/app/testetcnicoshopper/Model/entities/DriveEntities/DriverModel.kt
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e15a994cf958326283640e7d74f7d184f948ac37107faa04448ad033da7e6187
-size 182
+package com.app.testetcnicoshopper.Model.entities.DriveEntities
+
+data class DriverModel (
+    val customer_id : String,
+    val origin : String,
+    val destination : String,
+)
