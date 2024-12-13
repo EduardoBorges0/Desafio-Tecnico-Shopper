2d476f427234611b593712435f6489426283fee6
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:16:17 -0300

    Update FilterDrivers.kt

--- app/src/main/java/com/app/testetcnicoshopper/View/travelHistory/filter/FilterDrivers.kt
+++ app/src/main/java/com/app/testetcnicoshopper/View/travelHistory/filter/FilterDrivers.kt
@@ -1,3 +1,44 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:856adab7dd2cef5bec22d2433b1cb4d1bb65e8490339b4597cdfe53db9b34a4a
-size 1351
+package com.app.testetcnicoshopper.View.travelHistory.filter
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.app.testetcnicoshopper.ui.theme.DarkGreen
+
+@Composable
+fun ButtonFilter(
+    modifier: Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        ElevatedButton(
+            enabled = enabled,
+            onClick = onClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+                .height(50.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = DarkGreen,
+            )
+        ) {
+            Text(text, color = Color.White)
+        }
+
+
+    }
+}
