b0c435922e8bf4b89c9284f6261e13fb78419f1c
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:18:07 -0300

    Update TripPATCH.kt

--- app/src/main/java/com/app/testetcnicoshopper/Model/RetroFit/ConfirmTripAPI/TripPATCH.kt
+++ app/src/main/java/com/app/testetcnicoshopper/Model/RetroFit/ConfirmTripAPI/TripPATCH.kt
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eeac044a0f202c574481bcb347de165878bb823cb271e0aed62ab533727e9cde
-size 1043
+package com.app.testetcnicoshopper.Model.RetroFit.ConfirmTripAPI
+
+import com.app.testetcnicoshopper.Model.DTO.TripDTO.ResponseTripDTO
+import com.app.testetcnicoshopper.Model.DTO.TripDTO.TripDTO
+import com.app.testetcnicoshopper.Model.entities.TripEntities.TripResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.PATCH
+
+interface ConfirmTripAPI {
+    @PATCH("ride/confirm")
+    suspend fun confirmTrip(@Body confirmTripModel: TripDTO) : ResponseTripDTO
+
+    companion object{
+        val retroFitInstance : ConfirmTripAPI by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://xd5zl5kk2yltomvw5fb37y3bm40vsyrx.lambda-url.sa-east-1.on.aws/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            retrofit.create(ConfirmTripAPI::class.java)
+        }
+        fun getInstance() : ConfirmTripAPI {
+            return retroFitInstance
+        }
+    }
+}
