e9916704d1bedda28b4bdd5df843b6773f081af9
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:13:41 -0300

    Update Loading.kt

--- app/src/main/java/com/app/testetcnicoshopper/View/components/Loading.kt
+++ app/src/main/java/com/app/testetcnicoshopper/View/components/Loading.kt
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5e5c76c125c6aa6410238794af6883c07dd4cc6e9f4951b1f97e002891a199c9
-size 744
+package com.app.testetcnicoshopper.View.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.app.testetcnicoshopper.ui.theme.LightGreen
+
+@Composable
+fun Loading() {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Gray.copy(alpha = 0.3f))){
+        CircularProgressIndicator(
+            modifier = Modifier
+                .align(Alignment.Center),
+            color = LightGreen
+        )
+    }
+}
+
