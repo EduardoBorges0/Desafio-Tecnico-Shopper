1bddc4dbc1d19d8997200a01c948f50234870932
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:12:59 -0300

    Update CustomTextField.kt

--- app/src/main/java/com/app/testetcnicoshopper/View/components/CustomTextField.kt
+++ app/src/main/java/com/app/testetcnicoshopper/View/components/CustomTextField.kt
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b4ccb0cbfedb9242052582195fd8982c868bfb875d71b445cec7d43fee6550e
-size 1115
+package com.app.testetcnicoshopper.View.components
+
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    enabled: Boolean = true,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        enabled = enabled,
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        shape = RoundedCornerShape(8.dp),
+        colors = TextFieldDefaults.colors(
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            focusedLabelColor = Color.DarkGray,
+            unfocusedLabelColor = Color.Gray
+        ),
+        modifier = modifier.width(350.dp)
+    )
+}
