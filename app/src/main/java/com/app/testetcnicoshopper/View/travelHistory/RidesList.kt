ed1eeccc2df2933a47bfd85593430e808e7a2a95
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:14:54 -0300

    Update RidesList.kt

--- app/src/main/java/com/app/testetcnicoshopper/View/travelHistory/RidesList.kt
+++ app/src/main/java/com/app/testetcnicoshopper/View/travelHistory/RidesList.kt
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:524765caead9030b979fcb785883e659259eed7a559ce9caf82a359e853ee15f
-size 1257
+package com.app.testetcnicoshopper.View.travelHistory
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.app.testetcnicoshopper.Model.DTO.TravelListDTO.RidesDTO
+import com.app.testetcnicoshopper.View.components.formatDateAndTime
+
+@Composable
+fun RidesList(rides: List<RidesDTO>?) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        items(rides?.size ?: 0) {
+            val ride = rides?.get(it)
+            CardDriveComponent(
+                name = ride?.driver?.name ?: "",
+                value = ride?.value ?: 0.0,
+                distance = ride?.distance ?: 0.0,
+                duration = ride?.duration ?: "",
+                origin = ride?.origin ?: "",
+                destination = ride?.destination ?: "",
+                time = formatDateAndTime(ride?.date.toString()).second.toString(),
+                date = formatDateAndTime(ride?.date.toString()).first.toString()
+            )
+        }
+    }
+}
