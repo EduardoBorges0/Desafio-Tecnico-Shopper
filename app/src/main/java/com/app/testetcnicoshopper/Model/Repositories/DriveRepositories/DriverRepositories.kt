be4fc69b5e5d9be361e68ccab0323d16ee726b39
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:09:41 -0300

    Update DriverRepositories.kt

--- app/src/main/java/com/app/testetcnicoshopper/Model/Repositories/DriveRepositories/DriverRepositories.kt
+++ app/src/main/java/com/app/testetcnicoshopper/Model/Repositories/DriveRepositories/DriverRepositories.kt
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8463ebf0c8bfb225307b0a84a2d5752555b31b23fc980d3a4d581d320193c9da
-size 635
+package com.app.testetcnicoshopper.Model.Repositories.DriveRepositories
+
+import android.util.Log
+import com.app.testetcnicoshopper.Model.entities.DriveEntities.RouteResponse
+import com.app.testetcnicoshopper.Model.RetroFit.SearchDriveAPI.DrivePOST
+import com.app.testetcnicoshopper.Model.DTO.DriverDTO.DriverDTO
+import retrofit2.Response
+
+open class DriverRepositories(private val drivePOST: DrivePOST) {
+     open suspend fun SearchDriver(DriverDTO: DriverDTO): Response<RouteResponse> {
+        Log.d("RESPONSE REPOSITORIES", "RESPONSE REPOSITORIES ${DriverDTO}")
+        return drivePOST.searchDriver(DriverDTO)
+    }
+}
