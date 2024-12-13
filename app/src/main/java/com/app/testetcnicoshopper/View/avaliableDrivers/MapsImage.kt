aaca20d577afa65f6b68b93b39cb0f6d903e3cbe
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:11:58 -0300

    Update MapsImage.kt

--- app/src/main/java/com/app/testetcnicoshopper/View/avaliableDrivers/MapsImage.kt
+++ app/src/main/java/com/app/testetcnicoshopper/View/avaliableDrivers/MapsImage.kt
@@ -1,3 +1,53 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:602379a4379f28d33ea6f843689483e85e8e83872b37c242cbf383eea1522051
-size 2168
+package com.app.testetcnicoshopper.View.avaliableDrivers
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import coil.compose.rememberAsyncImagePainter
+import com.app.testetcnicoshopper.BuildConfig
+import com.app.testetcnicoshopper.Model.entities.DriveEntities.RouteResponse
+import retrofit2.Response
+
+fun mapsImage(latitudeOrigin: Double, longitudeOrigin: Double, latitudeDestination: Double, longitudeDestination: Double): String {
+    val apiKey = BuildConfig.API_KEY
+    val imageUrl = "https://maps.googleapis.com/maps/api/staticmap?size=500x500" +
+            "&markers=color:red|$latitudeOrigin,$longitudeOrigin" + // Marcador de origem
+            "&markers=color:blue|$latitudeDestination,$longitudeDestination" + // Marcador de destino
+            "&path=color:0x0000ff|weight:6|$latitudeOrigin,$longitudeOrigin|$latitudeDestination,$longitudeDestination" + // Caminho entre origem e destino
+            "&key=${apiKey}"
+
+    return imageUrl
+}
+
+@Composable
+fun Map(
+    response: LiveData<Response<RouteResponse>>,
+    modifier: Modifier
+){
+    val mapImage = response.value?.body()?.let { route ->
+        val originMap = route.origin
+        val destinationMap = route.destination
+        mapsImage(
+            latitudeOrigin = originMap.latitude,
+            longitudeOrigin = originMap.longitude,
+            latitudeDestination = destinationMap.latitude,
+            longitudeDestination = destinationMap.longitude
+        )
+    }
+    Image(
+        painter = rememberAsyncImagePainter(mapImage),
+        contentDescription = "Static Map API",
+        modifier = modifier
+            .padding(top = 130.dp)
+            .width(320.dp)
+            .height(320.dp)
+            .clip(RoundedCornerShape(57.dp))
+    )
+}
