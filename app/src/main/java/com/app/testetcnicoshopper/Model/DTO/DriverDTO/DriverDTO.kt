65caee23a74b84f83333cbbf5f9ef5fa0fdd7ae2
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-12-13 00:05:08 -0300

    Update DriverDTO.kt

--- app/src/main/java/com/app/testetcnicoshopper/Model/DTO/DriverDTO/DriverDTO.kt
+++ app/src/main/java/com/app/testetcnicoshopper/Model/DTO/DriverDTO/DriverDTO.kt
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9b404ab833089a1c3f88c73003289d52e9c1e3fe0e74166f560b0c89c8e60854
-size 622
+package com.app.testetcnicoshopper.Model.DTO.DriverDTO
+
+import com.app.testetcnicoshopper.Model.entities.DriveEntities.DriverModel
+
+data class DriverDTO(
+    val customer_id: String,
+    val origin: String,
+    val destination: String
+)
+
+fun DriverModel.toDTO(): DriverDTO {
+    return DriverDTO(
+        customer_id = this.customer_id,
+        origin = this.origin,
+        destination = this.destination
+    )
+}
+
+fun DriverDTO.toModel(): DriverModel {
+    return DriverModel(
+        customer_id = this.customer_id,
+        origin = this.origin,
+        destination = this.destination
+    )
+}
