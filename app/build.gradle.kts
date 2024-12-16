6c5b09a6f73c70774f30a05f9fdb4f08bfa793e3
EduardoBorges0 <gamr534@gmail.com>
2024-12-16 15:01:07 -0300

    Tests DriverViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70cc618749c48353cba9051c5514fb55a451f30ed7fe3caead8a41287d67129c
-size 2838
+oid sha256:719c588818c1780adb503cf254fc9a0522c75c5f95e8090c70af0268344ad872
+size 2811
--- app/src/main/java/com/app/testetcnicoshopper/ViewModel/DriveViewModel/DriveViewModel.kt
+++ app/src/main/java/com/app/testetcnicoshopper/ViewModel/DriveViewModel/DriveViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee3f501f7c977905b2b95ce590dde3e654537ed07052a8c70fd76fb43dc99fbc
-size 2746
+oid sha256:367e3d8bb04810c75a9f7443923b2706d0c4d32c6a4c4e88c8b56788af2784a2
+size 2848
